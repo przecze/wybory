4fc9d1f8d9ac1c421267796e55f68275fe42200d
Jan Czechowski <przemyslaw.czechowski@gmail.com>
2026-02-14 11:50:27 +0000

    footer and language switching tweaks

--- app.py
+++ app.py
@@ -340,17 +340,12 @@ def simulator(df):
 if __name__ == '__main__':
     st.set_page_config(layout="wide", page_title=t('page_title'), page_icon="🇵🇱")
 
-    # ── Language toggle: compact flag in top-right corner ─────────
-    _spacer, _lang_col = st.columns([0.92, 0.08])
-    with _lang_col:
-        _other = "en" if get_lang() == "pl" else "pl"
-        _flag = "🇬🇧" if _other == "en" else "🇵🇱"
-        if st.button(_flag, help=t('lang_toggle')):
-            st.query_params["lang"] = _other
-            st.rerun()
-
     lists, df = load_results()
     st.header(t('page_header'))
+    if get_lang() == 'pl':
+        st.markdown('🇬🇧 <a href="?lang=en" target="_self">Click here for English version</a>', unsafe_allow_html=True)
+    else:
+        st.markdown('🇵🇱 <a href="?lang=pl" target="_self">Kliknij tutaj, aby przejść na wersję polską</a>', unsafe_allow_html=True)
     df['seat_won'] = get_dhondt_seats(df, mandates_per_district)
     views = {
         t('view_select'): lambda *args:None,
@@ -364,9 +359,16 @@ if __name__ == '__main__':
 
 
     # Add a footer
-    footer_html = """
+    _lang = get_lang()
+    _gh_icon = '<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="vertical-align: middle; margin-right: 2px;"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>'
+    _src_text = "see source code" if _lang == "en" else "kod źródłowy"
+    if _lang == "pl":
+        _lang_switch = 'pl | <a href="?lang=en" target="_self">en</a>'
+    else:
+        _lang_switch = '<a href="?lang=pl" target="_self">pl</a> | en'
+    footer_html = f"""
         <div style="position: fixed; bottom: 0; left: 0; right: 0; background-color: lightgray; padding: 10px; text-align: center;">
-            <p style="margin: 10px 0; color: black;">&copy; 2023 Jan Czechowski | <a href="https://janczechowski.com" target="_blank">janczechowski.com</a> | <a href="https://github.com/przecze/wybory" target="_blank"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="20"/></a> | Data source: <a href="https://sejmsenat2023.pkw.gov.pl/sejmsenat2023/pl/dane_w_arkuszach" target="_blank">PKW</a></p>
+            <p style="margin: 10px 0; color: black;">({_lang_switch}) | &copy; 2026 Jan Czechowski | <a href="https://janczechowski.com" target="_blank">janczechowski.com</a> | <a href="https://github.com/przecze/wybory" target="_blank" style="vertical-align: middle;">{_gh_icon} {_src_text}</a> | Data source: <a href="https://sejmsenat2023.pkw.gov.pl/sejmsenat2023/pl/dane_w_arkuszach" target="_blank">Państwowa Komisja Wyborcza</a></p>
         </div>
     """
 
